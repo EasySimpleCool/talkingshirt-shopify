bae71e3b81eecffb870c1d91283ed8f78b1c0f94
EasySimpleCool <easysimplecool@gmail.com>
2022-08-24 00:53:04 +1000

    asd

--- comp/layout.tsx
+++ comp/layout.tsx
@@ -2,15 +2,14 @@ import React from 'react'
 import { motion } from "framer-motion"
 
 
-const Layout = ({ children }) => {
+const Layout = () => {
     return (
         <div>
             <motion.div
                 whileHover={{ scale: 1.1 }}
                 whileTap={{ scale: 1.1 }}
-            >this is at the top</motion.div>
-
-            {children}
+            >this is at the top
+            </motion.div>
             this is at the bottom
         </div>
     )
